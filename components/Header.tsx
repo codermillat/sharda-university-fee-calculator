c58fea0782f83b7301a86aaa029f093227482b3f
MD MILLAT HOSEN <codermillat@gmail.com>
2025-10-29 17:49:52 +0530

    Fix logo path to use BASE_URL for GitHub Pages

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,7 +6,7 @@ const Header: React.FC = () => {
       <div className="max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8 flex flex-col sm:flex-row items-center justify-between">
         <div className="flex items-center mb-2 sm:mb-0">
           <img 
-            src="/logo.png" 
+            src={`${import.meta.env.BASE_URL}logo.png`}
             alt="Sharda University Logo" 
             className="h-14 mr-4"
           />
