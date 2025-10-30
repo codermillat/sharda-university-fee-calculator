192a05ef1eb050e63bdf1805047e8cb0d82f4ae5
MD MILLAT HOSEN <codermillat@gmail.com>
2025-10-30 13:07:11 +0530

    Fix base path - use / for both Vercel and GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,9 @@ import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ command }) => {
+export default defineConfig(() => {
     return {
-      base: command === 'build' ? '/sharda-university-fee-calculator/' : '/',
+      base: '/',
       server: {
         port: 3000,
         host: 'localhost',
